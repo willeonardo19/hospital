d2f9a94664d080fcd3d62fcd0669cec953cebe42
WilLeonardo <willeonardo19@gmail.com>
2017-06-01 11:53:31 -0600

    proyecto ingeproyectos

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -32,10 +32,10 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
                 @if(Auth::user()->type =='admin' )
-                <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
+                <!--li class="header">{{ trans('adminlte_lang::message.header') }}</li-->
                 <!-- Optionally, you can add icons to the links -->
                 <li class="active"><a href="{{ url('home') }}"><i class='fa fa-home'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-                <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+                <!--li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li-->
                 <li class="treeview">
                     <a href="#"><i class='fa fa-user'></i> <span> Admin </span> <i class="fa fa-angle-left pull-right"></i></a>
                     <ul class="treeview-menu">
